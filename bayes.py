9e8eefa22b9928fc3c3affa81360610e2cea0ac8
trelkins3 <trelkins3@gmail.com>
2018-06-03 15:05:15 -0700

    Vocabulary creation

--- bayes.py
+++ bayes.py
@@ -1,1 +1,24 @@
 import numpy as np
+import string
+
+file = open('trainingSet.txt')
+
+vocabulary = []
+
+for line in file:
+	# Tokenize the line
+	buffer = line.split()
+	
+	for object in buffer:
+		# Strip punctuation
+		object = object.translate(None, string.punctuation)
+		if object not in vocabulary:
+			vocabulary.append(object)
+
+# Alphabetize
+vocabulary.sort()
+
+for thing in vocabulary:
+	print thing
+
+file.close()
