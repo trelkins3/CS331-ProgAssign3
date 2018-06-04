e5a2265326e299d00b478b6d39070dd05173545f
loughrao <loughrao@oregonstate.edu>
2018-06-04 04:21:33 -0700

    Smart made good code, 0.953

--- bayes.py
+++ bayes.py
@@ -119,18 +119,73 @@ for i in range(0,len(vocabulary)-1):
 # Probabilities of positives and negatives features sets
 posprob1 = []
 posprob0 = []
-for i in range(0,len(possums)-1):
+for i in range(0,len(possums)):
 	posprob1.append(0)
 	posprob0.append(0)
 	posprob1[i] = (float(possums[i])+1)/(float(poscount)+2)
 	posprob0[i] = (float(poscount)-float(possums[i])+1)/(float(poscount)+2)
 negprob1 = []
 negprob0 = []
-for i in range(0,len(negsums)-1):
+for i in range(0,len(negsums)):
 	negprob1.append(0)
 	negprob0.append(0)
 	negprob1[i] = (float(negsums[i])+1)/(float(negcount)+2)
 	negprob0[i] = (float(negcount)-float(negsums[i])+1)/(float(negcount)+2)
+
+# Predict training data
+train.seek(0)
+results  = []
+i = 0
+for line in train:
+	postemp1 = 1
+	postemp0 = 1
+	negtemp1 = 1
+	negtemp0 = 1
+	postemp  = 1
+	negtemp  = 1
+	# Tokenize  the line
+	buffer = line.split()
+
+	# Remove the classlabel
+	buffer.pop(len(buffer)-1)
+
+	# Strip punctuation and make lower case
+	k = 0
+	for object in buffer:
+		buffer[k] = (object.translate(None, string.punctuation)).lower()
+		k += 1
+	
+	for j in range(0,len(vocabulary)-1):
+		if vocabulary[j] in buffer:
+			postemp1 = float(postemp1)*float(posprob1[j])
+			negtemp1 = float(negtemp1)*float(negprob1[j])
+		else:
+			postemp0 = float(postemp0)*float(posprob0[j])
+			negtemp0 = float(negtemp0)*float(negprob0[j])
+
+	postemp = float(postemp1)*float(postemp0)*float(poschance)
+	negtemp = float(negtemp1)*float(negtemp0)*float(negchance)
+
+	results.append(0)
+	if (float(postemp)>float(negtemp)):
+		results[i] = 1
+	else:
+		results[i] = 0
+
+	i += 1
+
+print("results: ",results)
+print("length: " ,len(results))
+
+correct = 0
+for i in range(0,total):
+	if (int(results[i]) == int(values[i])):
+		correct += 1
+
+accuracy = float(correct)/float(total)
+print("accuracy: ", accuracy)
+
+
 
 ##### Test section #####
 testvocab = []
